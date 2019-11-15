67f0176b48fa48651b206a79ddeaa782f9803ac3
Naga Bhanu Rajiv Chintalapati <bchintalapati@gcs.com>
2019-11-15 16:11:24 +0530

    first commit

---
+++ src/main/java/com/development/aws/beanstalk/AwsBeanstalkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.development.aws.beanstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBeanstalkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBeanstalkExampleApplication.class, args);
+	}
+
+}
